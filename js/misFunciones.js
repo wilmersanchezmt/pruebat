f9eb40d89b83c8e928668e0279a372fd559895eb
wilmersanchezmt <wilmer.sanchez.mt@correo.usa.edu.co>
2021-10-26 19:06:16 -0500

    first commit

---
+++ js/misFunciones.js
@@ -0,0 +1,24 @@
+function traerInformacionCategorias(){
+    $.ajax({
+        url:"http://localhost:8080/api/Category/all",
+        type:"GET",
+        datatype:"JSON",
+        success:function(respuesta){
+            console.log(respuesta);
+            pintarRespuesta(respuesta);
+        }
+    });
+}
+
+function pintarRespuesta(respuesta){
+
+    let myTable="<table>";
+    for(i=0;i<respuesta.length;i++){
+        myTable+="<tr>";
+        myTable+="<td>"+respuesta[i].name+"</td>";
+        myTable+="<td>"+respuesta[i].description+"</td>";
+        myTable+="</tr>";
+    }
+    myTable+="</table>";
+    $("#resultado1").html(myTable);
+}
